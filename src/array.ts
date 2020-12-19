66f89a27cc70ed9061e6257deb9622bba6cfec8d
andrijunaedi <andrijunaedi126@gmail.com>
2020-12-19 21:23:40 +0700

    Array : number, string and any

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+/**
+ * * Array number
+ * ? array hanya bisa diisi number
+ */
+let numberArray: number[];
+numberArray = [1, 2, 3];
+
+/**
+ * * Array string
+ * ? array hanya bisa diisi string
+ */
+let stringArray: string[];
+stringArray = ['andri', 'junaedi'];
+
+/**
+ * * Array any
+ * ? array bisa berisi apa aja
+ */
+let anyArray: any[];
+anyArray = [1, 'andri', true, {}];
