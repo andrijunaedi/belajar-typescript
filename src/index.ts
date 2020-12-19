0f620aff4ef730852d5de098067343b0df75637f
andrijunaedi <andrijunaedi126@gmail.com>
2020-12-19 18:08:13 +0700

    type data number, boolean, and any

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 /**
- * * Type Data String
+ * * String
+ * ? Type data yang hanya berisi string
  */
 let nama: string = 'Andri Junaedi';
 nama = 'Junaedi';
+
+/**
+ * * Number
+ * ? Type data yang hanya berisi angka (number)
+ */
+let umur: number;
+umur = 17;
+umur = 18;
+
+/**
+ * * Type Data Boolean
+ * ? Type data yang hanya berisi true atau false
+ */
+let isDeveloper: boolean;
+isDeveloper = false;
+isDeveloper = true;
+
+/**
+ * * Type Data Any
+ * ? Type data yang bisa dimasukan data apa aja
+ */
+let heroes: any = 'Iron Man';
+heroes = 18;
+heroes = [];
+heroes = {};
+heroes = true || false || 'string';
