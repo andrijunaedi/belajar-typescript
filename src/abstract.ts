d37ba19d9f470a6211eda00fe71936843206cc0b
andrijunaedi <andrijunaedi126@gmail.com>
2020-12-23 23:39:55 +0700

    class: Abstract Class

---
+++ src/abstract.ts
@@ -0,0 +1,42 @@
+/**
+ * * Abstract Class
+ * ? Class yang tidak bisa diintansiasi langsung dari luar class itu sendiri
+ * ? Melainkan harus extends sebuah child class nya
+ */
+abstract class Vihecle {
+  /**
+   * ? Nilai wheels didapat dari child class yang melakukan extends
+   */
+  abstract wheels: number;
+
+  start(): void {
+    console.log('brummmmmm....');
+  }
+}
+
+/**
+ * * class Car adalah Vihecle (kendaraan)
+ * ? Car harus memasukkan nilai wheels karena extends ke Vihecle
+ */
+class Car extends Vihecle {
+  wheels: number = 4;
+}
+
+/**
+ * * class Motorcycle adalah Vihecle (kendaraan)
+ * ? Motorcycle harus memasukkan nilai wheels karena extends ke Vihecle
+ */
+class Motorcycle extends Vihecle {
+  wheels: number = 2;
+}
+
+/**
+ * * Example
+ */
+let car = new Car();
+console.log(car.wheels);
+car.start();
+
+let motor = new Motorcycle();
+console.log(motor.wheels);
+motor.start();
