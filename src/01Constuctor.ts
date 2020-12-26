b7cd8f25828d4b79da1749d71facea6d8939f1ba
andrijunaedi <andrijunaedi126@gmail.com>
2020-12-27 05:12:25 +0700

    Constructor

---
+++ src/01Constuctor.ts
@@ -0,0 +1,11 @@
+class Animal2 {
+  /**
+   * * Constructor
+   * ? Method khusus yang akan selalu dipanggil pada saat pembuatan class
+   */
+  constructor() {
+    console.log('constructor akan selalu dipanggil terlebih dahulu');
+  }
+}
+
+let bird = new Animal2();
